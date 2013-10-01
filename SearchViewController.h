266e4ebfd7f9dd3c439e53ab4f789abd6e9be1df
Gina Mullins <littlepeculiar13@yahoo.com>
2013-09-30 19:00:44 -0700

    Update SearchViewController.h

--- SearchViewController.h
+++ SearchViewController.h
@@ -7,39 +7,49 @@
 //
 
 #import <UIKit/UIKit.h>
-#import "SDSegmentedControl.h"
 
 
 
 @class ClubInfo;
-
+@class Database;
+@class Location;
 
 
 @interface SearchViewController : UIViewController
 <UITableViewDataSource, UITableViewDelegate, UITextFieldDelegate>
 
-
+@property (nonatomic, strong) Database *database;
+@property (nonatomic, strong) Location *location;
 @property (nonatomic, strong) NSMutableArray *listArray;
+@property (nonatomic, strong) NSMutableArray *allCities;
+@property (nonatomic, strong) NSMutableArray *autoCompleteArray;
 @property (nonatomic, copy) NSString *sectionHeader;
 @property (nonatomic, assign) SearchSelected listSelected;
+@property (nonatomic, assign) float searchLat;
+@property (nonatomic, assign) float searchLon;
+@property (nonatomic, assign) NSInteger tabIndex;
 
 // UI
 
-@property (weak, nonatomic) IBOutlet UISegmentedControl *segmentedControl;
+@property (weak, nonatomic) IBOutlet UIView *containerView;
 @property (weak, nonatomic) IBOutlet UITableView *listTable;
 @property (weak, nonatomic) IBOutlet UITextField *locationTextField;
 @property (weak, nonatomic) IBOutlet UIButton *mapButton;
+@property (weak, nonatomic) IBOutlet UITabBar *tabBar;
+@property (strong, nonatomic) UITableView *autoCompleteTable;
 
 
 - (IBAction)goBack:(id)sender;
 - (IBAction)mapAction:(id)sender;
 - (IBAction)GPSAction:(id)sender;
-- (IBAction)showSelectedView:(id)sender;
+- (IBAction)showAutoCompleteTable:(id)sender;
 
-- (void)loadListTable:(SearchSelected)buttonSelected;
+- (void)loadListTable:(SearchSelected)selected;
 - (NSInteger)sportNameIndexForLeague:(NSString*)sport;
 - (void)showAlert:(NSInteger)tag;
-
+- (void)createDataForAutoComplete;
+- (void)resetAutoCompleteTable;
 
 
 @end
+
